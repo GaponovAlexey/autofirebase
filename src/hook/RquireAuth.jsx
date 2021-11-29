da3650ede19b611a18efe9a1d8995cdb4b083f01
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-11-29 17:25:13 +0300

    save

--- src/hook/RquireAuth.jsx
+++ src/hook/RquireAuth.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useLocation, Navigate } from 'react-router-dom'
+import { useAuth } from './use-auth'
 
-export const RquireAuth = ({children}) => {
-  const location = useLocation()
-  const {isAuth} = useSelector(state => state.user)
-  const auth = isAuth
+// export const RquireAuth = ({children}) => {
+//   const location = useLocation()
+//   const {isAuth} = useSelector(state => state.user)
+//   const auth = isAuth
   
-  if (!auth) {
-    return <Navigate to='/login' state={{ from: location }} />
+//   if (!auth) {
+//     return <Navigate to='/login' state={{ from: location }} />
+//   }
+//   return children
+// }
+                  
+export const RquireAuth = ({children}) => {
+  const {isAuth} = useAuth()
+  if (!isAuth) {
+    return <Navigate to='/login'/>
   }
   return children
 }
-                  
